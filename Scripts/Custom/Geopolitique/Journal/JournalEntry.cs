38d679279c35d8b87e73596ec3e3481590480dd1
Trickster <dav.thibodeau@gmail.com>
2014-01-09 18:26:42 -0500

    Premier ajout pour le journal des evenements de la geopolitique

---
+++ Scripts/Custom/Geopolitique/Journal/JournalEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Systemes.Geopolitique.Journal
+{
+    public class JournalEntry
+    {
+        private Mobile m_Createur;
+
+        public Mobile Createur { get { return m_Createur; } }
+
+        public JournalEntry(Mobile from)
+        {
+            m_Createur = from; 
+        }
+    }
+
+    public class CreerCategorieEntry : JournalEntry
+    {
+        private Categorie categorie;
+
+        public CreerCategorieEntry(Mobile from, Categorie cat)
+            : base(from)
+        {
+            categorie = cat;
+        }
+
+    }
+
+}
