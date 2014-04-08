8ff3085e23e129b441d7ba58568129c012454eaa
Trickster <dav.thibodeau@gmail.com>
2014-04-07 22:45:19 -0400

    Correction du bug de teleport et rajout du sort.
    Le fichier tiledata est modifié de façon à ce que tous les murs aient
    soit noshoot soit window qui sont les flags testés par LineOfSight.

--- Scripts/Custom/Spells/Third/Teleport.cs
+++ Scripts/Custom/Spells/Third/Teleport.cs
@@ -26,15 +26,13 @@ namespace Server.Spells.Third
 
 		public override bool CheckCast()
 		{
-            //if ( Server.Misc.WeightOverloading.IsOverloaded( Caster ) )
-            //{
-            //    Caster.SendLocalizedMessage( 502359, "", 0x22 ); // Thou art too encumbered to move.
-            //    return false;
-            //}
+            if (Server.Misc.WeightOverloading.IsOverloaded(Caster))
+            {
+                Caster.SendLocalizedMessage(502359, "", 0x22); // Thou art too encumbered to move.
+                return false;
+            }
 
-            //return base.CheckCast(); //SpellHelper.CheckTravel( Caster, TravelCheckType.TeleportFrom );
-            Caster.SendMessage("Sort temporairement désactivé en raison de bug abuse.");
-            return false;
+            return base.CheckCast(); //SpellHelper.CheckTravel( Caster, TravelCheckType.TeleportFrom );
 
 		}
 
