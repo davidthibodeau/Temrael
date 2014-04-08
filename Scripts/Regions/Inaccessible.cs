997a47a4740e62fe53c5b9c00554c070492820a2
Trickster <dav.thibodeau@gmail.com>
2014-04-07 23:27:18 -0400

    Ajout d'une region pour bloquer les teleports a certaines regions de map.
    Cette région empêche les joueurs d'utiliser teleport pour accéder au chemin
    vers l'autre continent. Éventuellement, l'autre continent entier devrait
    être dans cette region et la région devrait empêcher l'utilisation des
    runes qui pointent vers l'intérieur de la région (puisque des joueurs en
    ont sans doute déjà).

---
+++ Scripts/Regions/Inaccessible.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Server.Regions
+{
+    public class Inaccessible : Region
+    {
+        //Region utilisee pour les parties de map bloquees.
+        public Inaccessible(XmlElement xml, Map map, Region parent)
+            : base(xml, map, parent)
+        {
+        }
+
+        public override bool AllowSpawn()
+        {
+            return false;
+        }
+    }
+}
