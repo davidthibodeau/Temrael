44f7aee1d1a8995cddb563f218fca56c77918a91
Trickster <dav.thibodeau@gmail.com>
2014-06-08 22:58:53 -0400

    Mise à jour de Server/AssemblyInfo.cs

--- Server/AssemblyInfo.cs
+++ Server/AssemblyInfo.cs
@@ -5,7 +5,7 @@
  *   copyright            : (C) The RunUO Software Team
  *   email                : info@runuo.com
  *
- *   $Id: AssemblyInfo.cs 644 2010-12-23 09:18:45Z asayre $
+ *   $Id$
  *
  ***************************************************************************/
 
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RunUO Server Core")]	//Having just RunUO there is reundant, ie, RunUO.exe with the word 'RunUO' under it
 [assembly: AssemblyDescription("UO Server Software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The RunUO Team")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("RunUO")]
+[assembly: AssemblyProduct("RunUO")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.6.0.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
