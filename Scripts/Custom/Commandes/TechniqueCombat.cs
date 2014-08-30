4711c76e80bddba2cdc5f484380916239e685d28
Zilmerx <hex404@gmail.com>
2014-08-30 05:50:42 -0400

    Base de technique de combat.
    Une petite classe qui pourra servir de base pour des techniques de
    combat, si jamais nous en faisons. Pour le moment, la commande est
    non-fonctionnelle puisqu'elle modifie l'attack speed de toutes les armes
    du même type. Je la laisse là si jamais quelqu'un a le goût de s'amuser
    à la modifier, car je risque d'être un peu absent pour les prochains
    jours.
    
    J'ai mis l'access level à Batisseur, pour éviter un oubli et que ça soit
    accessible aux joueurs au release.

---
+++ Scripts/Custom/Commandes/TechniqueCombat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using Server;
+using Server.Gumps;
+using Server.Mobiles;
+using Server.Commands;
+using Server.TechniquesCombat;
+
+namespace Server.Custom.Commandes
+{
+    class TechniquesCombat
+    {
+        public static void Initialize()
+        {
+
+            CommandSystem.Register("Technique1", AccessLevel.Batisseur, new CommandEventHandler(Technique1_OnCommand));
+
+            // ...
+        }
+
+        [Usage("Technique1")]
+        [Description("Confère un bonus sur la vitesse d'attaque.")]
+        public static void Technique1_OnCommand(CommandEventArgs e)
+        {
+            e.Mobile.Say("Technique1");
+            new AttackSpeed((PlayerMobile)e.Mobile);
+        }
+
+        // ...
+    }
+}
---
+++ Scripts/Custom/Spells/TechniquesCombat/AttackSpeed.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using Server.Mobiles;
+using Server.Items;
+
+namespace Server.TechniquesCombat
+{
+    public class AttackSpeed
+    {
+
+        // Ressources
+        private PlayerMobile m_Caster;
+        public AttackSpeed(PlayerMobile caster)
+        {
+            m_Caster = caster;
+            Use();
+        }
+
+
+        // Modifiables.
+        private int m_ManaCost = 10;
+        private const float m_MultiplicateurSpeed = 5f;
+
+
+        TimeSpan CalculDuree()
+        {
+            int Hours = 0;
+            int Minutes = 0;
+            int Seconds = 0;
+
+            // Insert formula here...
+            Seconds = 20;
+
+            return new TimeSpan(Hours, Minutes, Seconds);
+        }
+
+
+        private void Use()
+        {
+            if ((!m_Caster.Deleted && m_Caster.Alive))
+            {
+                if (m_Caster.Mana >= m_ManaCost)
+                {
+                    m_Caster.Mana -= m_ManaCost;
+                    BonusTimer m_TimerDuration = new BonusTimer(CalculDuree(), m_Caster);
+                }
+                else
+                {
+                    m_Caster.SendMessage("Vous n'avez pas assez de mana pour utiliser cette technique !");
+                }
+            }
+        }
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Timer qui ajoute un bonus, puis le retire quand le temps est écoulé.
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        private class BonusTimer : Timer
+        {
+            private PlayerMobile m_Caster;
+            private BaseWeapon m_Weapon;
+
+
+
+
+            private void AjoutBonus()
+            {
+                m_Weapon.Speed = m_Weapon.Speed / m_MultiplicateurSpeed;
+            }
+
+
+            private void RetraitBonus()
+            {
+                m_Weapon.Speed = m_Weapon.Speed * m_MultiplicateurSpeed;
+            }
+
+
+            public BonusTimer(TimeSpan duration, PlayerMobile caster)
+                : base(duration)
+            {
+                m_Caster = caster;
+                m_Weapon = (BaseWeapon)caster.Weapon;
+
+                Priority = TimerPriority.TwoFiftyMS;
+
+                CommencerTechnique();
+            }
+
+            // Pas touche ---v
+
+            private bool DetruitCorrectement;
+
+            public void CommencerTechnique()
+            {
+                AjoutBonus();
+                DetruitCorrectement = false;
+                Start();
+            }
+
+            protected override void OnTick()
+            {
+                RetraitBonus();
+                DetruitCorrectement = true;
+                Stop();
+            }
+
+            ~BonusTimer()
+            {
+                if (DetruitCorrectement == false)
+                {
+                    RetraitBonus();
+                }
+            }
+        }
+    }
+}
