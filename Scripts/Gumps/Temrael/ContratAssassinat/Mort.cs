7af26aa376e0fffd401c5082208cdc6d895eda7c
Zilmerx <hex404@gmail.com>
2014-10-26 15:51:31 -0400

    Second gump pour la mort, pour prendre commentaire.

--- Scripts/Gumps/Temrael/ContratAssassinat/Mort.cs
+++ Scripts/Gumps/Temrael/ContratAssassinat/Mort.cs
@@ -54,9 +54,7 @@ namespace Server.Gumps
 
         public override void OnResponse(NetState sender, RelayInfo info)
         {
-            Mobile from = sender.Mobile;
-
-            if (from.Deleted)
+            if (m_From.Deleted)
                 return;
 
             switch (info.ButtonID)
@@ -64,34 +62,94 @@ namespace Server.Gumps
                 case 1:
                     m_cs.Cible.Corpse.PublicOverheadMessage(MessageType.Regular, 0x0, false, "La mort a été contestée.");
 
-                    m_cs.Cible.Resurrect(); // Resurect corpse ( Pour éviter que le cadavre decay )
+                    m_cs.Cible.Resurrect(); // Pour éviter que le cadavre decay..
 
                     // Teleport to jail.
 
                     ((TMobile)m_cs.Cible).Mort = false;
                     ((TMobile)m_cs.Cible).MortCurrentState = MortState.Aucun;
 
-                    // Make forum ticket.
-                    PhpBB forumPost = new PhpBB("LEUSERNAME", "LEPASSWORD");
-
-                    //
-
-                    m_cs.Cible.SendMessage("Une demande de contestation a été envoyée à l'équipe, et sera traitée dans les plus brefs délais !");
+                    m_cs.Cible.SendGump(new MortContestGump(m_From, m_cs, m_listePersoPresent));
 
                     break;
                 case 2:
-                    
                     m_cs.Cible.Corpse.PublicOverheadMessage(MessageType.Regular, 0x0, false, "La mort a été acceptée.");
 
                     // Teleport to cemetary.
 
-                    // Write sad message. RIP.
                     m_cs.Cible.SendMessage("C'est ainsi que se termine l'histoire de " + m_cs.Cible.Name + ". RIP.");
 
                     break;
 
                 default: break;
             }
+
+            m_cs.Cible.CloseGump(typeof(MortGump));
         }
+
+
+        class MortContestGump : Gump
+        {
+            private Mobile m_From;
+            private ContratAssassinat m_cs;
+            private List<Mobile> m_listePersoPresent = new List<Mobile>();
+
+            public MortContestGump(Mobile from, ContratAssassinat cs, List<Mobile> listePersoPresent)
+                : base(0, 0)
+            {
+                m_From = from;
+                m_cs = cs;
+                m_listePersoPresent = listePersoPresent;
+
+                Closable = false;
+                Disposable = true;
+                Dragable = true;
+                Resizable = false;
+
+                AddPage(0);
+
+                //BG
+                AddBackground(80, 72, 390, 400, 3600);
+                AddBackground(90, 82, 370, 380, 9200);
+                AddBackground(100, 92, 350, 355, 3500);
+                AddBackground(140, 230, 270, 120, 0xBB8);
+
+                AddHtml(140, 125, 270, 100, "Désirez-vous laisser un message ? \n\nIci vous pouvez décrire quelles ont été les circonstances de votre mort, et pourquoi vous souhaitez contester.", false, false);
+
+                AddTextEntry(140, 240, 265, 120, 0x7FA, 2, "");
+
+                AddHtml(220, 380, 100, 20, "Envoyer", false, false);
+                AddButton(280, 380, 0x481, 0x483, 1, GumpButtonType.Reply, 0);
+            }
+
+            public override void OnResponse(NetState sender, RelayInfo info)
+            {
+                Mobile from = sender.Mobile;
+
+                if (from.Deleted)
+                    return;
+
+                switch (info.ButtonID)
+                {
+                    case 1:
+
+                        String message = (info.GetTextEntry(2)).Text;
+
+                        // Make forum ticket.
+                        PhpBB forumPost = new PhpBB("LEUSERNAME", "LEPASSWORD");
+
+                        //
+
+                        m_cs.Cible.SendMessage("Une demande de contestation a été envoyée à l'équipe, et sera traitée dans les plus brefs délais !");
+
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+
+
+
     }
 }
