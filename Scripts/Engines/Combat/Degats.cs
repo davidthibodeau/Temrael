1b905d39bdff60a6fc126cd69fb559e446157ee2
Zilmerx <hex404@gmail.com>
2014-10-31 14:05:18 -0400

    Dégâts magiques.
    - J'ai mis en place le code qui fait en sorte qu'une branche différente
    de la branche que le spell utilise donne un bonus aux dégâts. Par
    contre, le code n'affecte présentement pas les dégâts, puisque les
    constantes sont à 0.

--- Scripts/Engines/Combat/Degats.cs
+++ Scripts/Engines/Combat/Degats.cs
@@ -8,6 +8,7 @@ namespace Server.Engines.Combat
 
         public static readonly Damage instance = new Damage();
         
+        #region Degats Physiques
         public double DegatsPhysiquesReduits(Mobile atk, Mobile def, double dmg)
         {
             return DegatsPhysiquesReduits(atk, def, dmg, 0);
@@ -22,13 +23,64 @@ namespace Server.Engines.Combat
 
             return Reduction(dmg, reducedar + def.ArmureNaturelle);
         }
+        #endregion
 
+        #region Degats Magiques
         public void AppliquerDegatsMagiques(Mobile def, double dmg)
         {
             double reducedDmg = Reduction(dmg, def.MagicResistance);
 
             def.Damage((int)reducedDmg);
         }
+
+        const double DPSBASE = 6.45;
+        const double ScalingArtMag      = 0.5;// Bonus lié au skill ArtMagique.
+        const double ScalingCategorie   = 0.5;// Bonus qui fait la différence entre un spell de cercle 1, et de cercle 10.
+        const double ScalMainBranche    = 0;  // Scaling sur le skill de la branche passée en paramètre.
+        const double ScalScndBranche    = 0;  // Scaling sur les skills des autres branches.
+        const short NbTotalCercles      = 10; // Il y a présentement 10 cercles dans le système de magie.
+
+        public double GetDegatsMagiques(Mobile atk, SkillName branche, short cercle, TimeSpan tempsCast)
+        {
+            double BaseDamage = BaseDegatsMagiques(cercle, tempsCast);
+            double TotalDamage = BaseDamage;
+
+            // Les ifs sont gérés à la compilation, donc pas de coût, juste un warning gossant.
+            if (ScalingArtMag != 0)
+            {
+                TotalDamage += GetBonus(BaseDamage, ScalingArtMag, 0);
+            }
+
+            if (ScalMainBranche != 0)
+            {
+                // "ScalMainBranche - ScalScndBranche" parce qu'on reprend l'influence de la branche principale comme une branche secondaire, plus tard.
+                TotalDamage += GetBonus(BaseDamage, ScalMainBranche - ScalScndBranche, 0) * atk.Skills[branche].Value;
+            }
+
+            if (ScalScndBranche != 0)
+            {
+                double value = atk.Skills[SkillName.Evocation].Value
+                             + atk.Skills[SkillName.Immuabilite].Value
+                             + atk.Skills[SkillName.Alteration].Value
+                             + atk.Skills[SkillName.Providence].Value
+                             + atk.Skills[SkillName.Transmutation].Value
+                             + atk.Skills[SkillName.Thaumaturgie].Value
+                             + atk.Skills[SkillName.Hallucination].Value
+                             + atk.Skills[SkillName.Ensorcellement].Value
+                             + atk.Skills[SkillName.Necromancie].Value;
+
+                TotalDamage += GetBonus(BaseDamage, ScalScndBranche, 0) * value;
+            }
+
+            return TotalDamage;
+        }
+
+        private double BaseDegatsMagiques(short cercle, TimeSpan tempsCast)
+        {
+            // Calcul de la fiche Excel.
+            return (((((ScalingCategorie / NbTotalCercles) * cercle) + 1) * DPSBASE) * tempsCast.TotalSeconds);
+        }
+        #endregion
 
         private double Reduction(double dmg, double resist)
         {
