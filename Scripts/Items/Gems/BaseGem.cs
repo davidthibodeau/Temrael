f60945bfe8c1c2da882f8d2f46ebe7b0ab1b68c0
Zilmerx <hex404@gmail.com>
2014-11-16 18:17:51 -0500

    Fix du serialize BaseGem

--- Scripts/Items/Gems/BaseGem.cs
+++ Scripts/Items/Gems/BaseGem.cs
@@ -33,5 +33,19 @@ namespace Server.Items
         public BaseGem(Serial serial)
             : base(serial)
         { }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
     }
 }
