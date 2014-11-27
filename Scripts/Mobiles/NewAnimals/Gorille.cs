26abcbbb11b573b69a5ea70a34c7264bbb73b634
Trickster <trickster@uotemrael.com>
2014-11-26 19:23:32 -0500

    Ajout d'une vague d'animaux.

---
+++ Scripts/Mobiles/NewAnimals/Gorille.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server.Mobiles.NewAnimals
+{
+
+    [CorpseName("Cadavre de gorille")]
+    public class Gorille : BaseCreature
+    {
+        [Constructable]
+        public Gorille()
+            : base(AIType.AI_Melee, FightMode.Aggressor, 8, 1, 0.2, 0.4)
+        {
+            Name = "Gorille";
+            Body = 29;
+            BaseSoundID = 0x09F;
+
+            PlayersAreEnemies = true;
+
+            SetStr(30);
+            SetDex(20);
+            SetInt(5);
+
+            SetHits(80);
+            SetMana(10);
+            SetStam(40);
+            SetArme(4, 7, 40);
+
+            SetResistance(ResistanceType.Physical, 0);
+            SetResistance(ResistanceType.Magie, 0);
+
+            SetSkill(SkillName.ArmureNaturelle, 46);
+            SetSkill(SkillName.Tactiques, 46);
+            SetSkill(SkillName.Epee, 46);
+            SetSkill(SkillName.Anatomie, 22);
+
+
+        }
+
+        public override int Hide { get { return 3; } }
+        public override HideType HideType { get { return HideType.Regular; } }
+
+        public Gorille(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int Meat { get { return 2; } }
+        public override MeatType MeatType { get { return MeatType.Ribs; } }
+
+        public Gorille(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
---
+++ Scripts/Mobiles/NewAnimals/Panther.cs
@@ -0,0 +1,68 @@
+using System;
+using Server.Mobiles;
+
+namespace Server.Mobiles
+{           
+    [CorpseName("un corps de panthere")]
+    public class Panthere : BaseCreature
+    {
+        [Constructable]
+        public Panthere()
+            : base(AIType.AI_Thief, FightMode.Aggressor, 8, 1, 0.2, 0.4)
+        {
+            Name = "Une panthere";
+
+            PlayersAreEnemies = true;
+            Direction = Direction.West;
+            Hidden = true;
+
+            SetStr(45);
+            SetDex(40);
+            SetInt(5);
+
+            SetHits(175);
+            SetMana(10);
+            SetStam(80);
+            SetArme(5, 9, 40);
+
+            SetResistance(ResistanceType.Physical, 0);
+            SetResistance(ResistanceType.Magie, 0);
+
+            SetSkill(SkillName.ArmureNaturelle, 48);
+            SetSkill(SkillName.Tactiques, 48);
+            SetSkill(SkillName.Epee, 48);
+            SetSkill(SkillName.Infiltration, 36);
+
+
+            Tamable = true;
+            ControlSlots = 3;
+            MinTameSkill = 40;
+
+        }
+
+        public override int Hide { get { return 2; } }
+        public override HideType HideType { get { return HideType.Regular; } }
+
+        public override int Meat { get { return 2; } }
+        public override MeatType MeatType { get { return MeatType.Ribs; } }
+
+        public Panthere(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
