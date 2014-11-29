5d0fdbdea15801d9a485d1efe01e8b82c3fddf5f
Trickster <trickster@uotemrael.com>
2014-11-28 23:55:53 -0500

    Correction du menu de race dans la fiche.

--- Scripts/Gumps/Temrael/Fiche/Connexe/FicheRacesInfoGump.cs
+++ Scripts/Gumps/Temrael/Fiche/Connexe/FicheRacesInfoGump.cs
@@ -11,9 +11,8 @@ using Server.Engines.Races;
 
 namespace Server.Gumps.Fiche
 {
-    public class FicheRacesInfoGump : GumpTemrael
+    public class FicheRacesInfoGump : BaseFicheGump
     {
-        private PlayerMobile m_from;
         private Race m_Race;
 
         public FicheRacesInfoGump(PlayerMobile from)
@@ -22,36 +21,14 @@ namespace Server.Gumps.Fiche
         }
 
         public FicheRacesInfoGump(PlayerMobile from, Race race)
-            : base("Race & Alignement", 560, 622)
+            : base(from, "Race & Alignement", 560, 622, 1)
         {
-            m_from = from;
             m_Race = race;
 
             int x = XBase;
             int y = YBase;
             int line = 0;
             int scale = 25;
-
-            y = 650;
-            x = 90;
-            int space = 80;
-
-            AddMenuItem(x, y, 1178, 1, false);
-            x += space;
-            AddMenuItem(x, y, 1179, 2, true);
-            x += space;
-            AddMenuItem(x, y, 1180, 3, true);
-            x += space;
-            AddMenuItem(x, y, 1194, 4, true);
-            x += space;
-            AddMenuItem(x, y, 1196, 5, true);
-            x += space;
-            AddMenuItem(x, y, 1222, 6, true);
-            x += space;
-            AddMenuItem(x, y, 1191, 7, true);
-
-            x = XBase;
-            y = YBase;
 
             AddTitre(x + 360, y + line * scale, 190, "Races");
             ++line;
@@ -86,29 +63,10 @@ namespace Server.Gumps.Fiche
             if (from.Deleted || !from.Alive)
                 return;
 
-            switch (info.ButtonID)
+            if (info.ButtonID < 8)
             {
-                case 1:
-                    from.SendGump(new FicheRaceGump(from));
-                    break;
-                case 2:
-                    from.SendGump(new FicheClasseGump(from));
-                    break;
-                case 3:
-                    from.SendGump(new FicheCaracteristiqueGump(from));
-                    break;
-                case 4:
-                    from.SendGump(new FicheCompetencesGump(from));
-                    break;
-                case 5:
-                    from.SendGump(new FicheStatistiquesGump(from));
-                    break;
-                case 6:
-                    from.SendGump(new FicheStatutsGump(from));
-                    break;
-                case 7:
-                    from.SendGump(new FicheCommandesGump(from));
-                    break;
+                base.OnResponse(sender, info);
+                return;
             }
 
             if (info.ButtonID >= 50)
