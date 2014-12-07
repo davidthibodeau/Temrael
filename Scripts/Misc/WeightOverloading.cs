e130f539f056e0b4f8f4945c8e2ce3752b66a58b
Zilmerx <hex404@gmail.com>
2014-12-07 03:59:25 -0500

    Modification du coût en stamina de la course.
    - Sera à tester un peu plus. Je ne serais pas contre un monde où le coût
    de la course serait encore doublé, et la regen de stamina doublée elle
    aussi.

--- Scripts/Misc/WeightOverloading.cs
+++ Scripts/Misc/WeightOverloading.cs
@@ -58,15 +58,15 @@ namespace Server.Misc
 			return m.MaxWeight;
 		}
 
+        const int PERTE_STAM_RUNNING = 2;
+        const int MULTI_TILE_ACCIDENTE = 3; // Il coûte 3 fois plus cher de courrir sur du terrain accidenté que non.
+
 		public static void EventSink_Movement( MovementEventArgs e )
 		{
             Mobile from = e.Mobile;
 
             if (!from.Player || !from.Alive || from.AccessLevel >= AccessLevel.Batisseur)
                 return;
-
-            int maxWeight = GetMaxWeight(from) + OverloadAllowance;
-            int overWeight = (Mobile.BodyWeight + from.TotalWeight) - maxWeight;
 
             if (from.StamMax == 0 || from.ManaMax == 0 || from.Dex <= 0)
             {
@@ -82,6 +82,7 @@ namespace Server.Misc
                 return;
             }
 
+            int overWeight = (Mobile.BodyWeight + from.TotalWeight) - (GetMaxWeight(from) + OverloadAllowance);
             if (overWeight > 0)
             {
                 from.Stam -= GetStamLoss(from, overWeight, (e.Direction & Direction.Running) != 0);
@@ -94,116 +95,24 @@ namespace Server.Misc
                 }
             }
 
-            if (((from.Stam * 100) / Math.Max(from.StamMax, 1)) < 1)
-            {
-                if ((e.Direction & Direction.Running) != 0 || from.Stam <= 1)
-                    from.Stam -= 2;
-            }
 
             if (from is PlayerMobile && (e.Direction & Direction.Running) != 0)
             {
                 PlayerMobile pm = (PlayerMobile)from;
-                TileType type = Deplacement.GetTileType(from);
-                int amt, perte;
-                int value = 0;
-                bool isActive = Deplacement.IsActive(from);
 
-                if (from.AccessLevel == AccessLevel.Player)
+                int PerteStam = PERTE_STAM_RUNNING;
+                int amt = (from.Mounted ?  4 : 3); // Nombre de pas de course avant de perdre de la stam.
+
+
+                if (Deplacement.IsActive(from) && Deplacement.GetTileType(from) != TileType.Other) // Si terrain accidenté.
                 {
-                    if (isActive)
-                    {
-                        switch (type)
-                        {
-                            // TOCHECK LIBREDEPLACEMENT
-                            case TileType.Desert: value = (int)(pm.Skills.Survie.Value); break;
-                            case TileType.Jungle: value = (int)(pm.Skills.Survie.Value); break;
-                            case TileType.Forest: value = (int)(pm.Skills.Survie.Value); break;
-                            case TileType.Snow: value = (int)(pm.Skills.Survie.Value); break;
-                            case TileType.Swamp: value = (int)(pm.Skills.Survie.Value); break;
-                        }
-                    }
+                    PerteStam *= (1 + MULTI_TILE_ACCIDENTE - (MULTI_TILE_ACCIDENTE * (int)(pm.Skills.Survie.Value) / 100));
                 }
 
-                /*if (pm.Ville == Ville.Ilidelwis && pm.Region != null && pm.Region is TerritoryRegion && ((TerritoryRegion)pm.Region).Races == Race.Daelwena)
-                    isActive = false;
 
-                if (pm.Ville == Ville.Najarhim && type == TileType.Desert)
-                    isActive = false;*/
-
-                amt = (from.Mounted ? 4 : 3);
-
-                if (isActive && type != TileType.Other)
-                {
-                    perte = ((value + 12) - (value * 2));
-
-                    if (value < 8 && Utility.Random(8) > value)
-                        from.Stam -= 1;
-
-                    if (value < 6 && Utility.Random(6) > value)
-                        from.Stam -= 1;
-
-                    if (value < 4 && Utility.Random(4) > value)
-                        from.Stam -= 1;
-
-                    amt += (from.Mounted ? 4 - perte / 2 : 3 - perte / 2);
-                }
-
-                if (amt < 1)
-                    amt = 1;
-
-                //amt += (int)(from.GetAttributValue(Attribut.Constitution) / 40);
-
-                if ((++pm.StepsTaken % amt) == 0)
-                    from.Stam -= 1;
+                if ((++pm.StepsTaken % amt) == 0) // À chaque "amt" cases, le joueur perd de la stam.
+                    from.Stam -= PerteStam;
             }
-
-			/*Mobile from = e.Mobile;
-
-			if ( !from.Alive || from.AccessLevel > AccessLevel.Player  )
-				return;
-
-			if ( !from.Player )
-			{
-				// Else it won't work on monsters.
-				//Spells.Ninjitsu.DeathStrike.AddStep( from );
-				return;
-			}
-
-			int maxWeight = GetMaxWeight( from ) + OverloadAllowance;
-			int overWeight = (Mobile.BodyWeight + from.TotalWeight) - maxWeight;
-
-			if ( overWeight > 0 )
-			{
-				from.Stam -= GetStamLoss( from, overWeight, (e.Direction & Direction.Running) != 0 );
-
-				if ( from.Stam == 0 )
-				{
-					from.SendLocalizedMessage( 500109 ); // You are too fatigued to move, because you are carrying too much weight!
-					e.Blocked = true;
-					return;
-				}
-			}
-
-			if ( ((from.Stam * 100) / Math.Max( from.StamMax, 1 )) < 10 )
-				--from.Stam;
-
-			if ( from.Stam == 0 )
-			{
-				from.SendLocalizedMessage( 500110 ); // You are too fatigued to move.
-				e.Blocked = true;
-				return;
-			}
-
-			if ( from is PlayerMobile )
-			{
-				int amt = ( from.Mounted ? 48 : 16 );
-				PlayerMobile pm = (PlayerMobile)from;
-
-				if ( (++pm.StepsTaken % amt) == 0 )
-					--from.Stam;
-			}
-
-			//Spells.Ninjitsu.DeathStrike.AddStep( from );*/
 		}
 
 		public static int GetStamLoss( Mobile from, int overWeight, bool running )
