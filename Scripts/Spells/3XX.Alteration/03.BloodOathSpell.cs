c052f4f09c253de13734af295ae318ac4712b5e2
Trickster <trickster@uotemrael.com>
2014-12-18 20:12:13 -0500

    Légere augmentation de la duree de bloodoath.

--- Scripts/Spells/3XX.Alteration/03.BloodOathSpell.cs
+++ Scripts/Spells/3XX.Alteration/03.BloodOathSpell.cs
@@ -72,7 +72,7 @@ namespace Server.Spells
 				m.FixedParticles( 0x375A, 1, 17, 9919, 33, 7, EffectLayer.Waist );
 				m.FixedParticles( 0x3728, 1, 13, 9502, 33, 7, (EffectLayer)255 );
 
-                double duration = 20;
+                double duration = 40;
 
                 duration *= Spell.GetSpellScaling(Caster, Info.skillForCasting);
 
