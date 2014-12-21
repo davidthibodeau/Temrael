6a902dd45002a718487ff1e0cbf09177dfc4e143
Zilmerx <hex404@gmail.com>
2014-12-21 04:53:37 -0500

    Ajout de .hohoho et d'un NPC dresseur.
    Les animaux dressés se font attaquer par les NPCs. Probablement un bug
    lié aux factions.

---
+++ Scripts/Commandes/Temrael/Hohoho.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Server;
+using Server.Gumps;
+using Server.Mobiles;
+using Server.Commands;
+using Server.Targeting;
+
+namespace Server.Commandes
+{
+    class Hohoho
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("Hohoho", AccessLevel.Player, new CommandEventHandler(Hohoho_OnCommand));
+        }
+
+        [Usage("Hohoho")]
+        [Description("Permet d'ajouter l'item clické au sac de tous des joueurs.")]
+        public static void Hohoho_OnCommand(CommandEventArgs e)
+        {
+            e.Mobile.Target = new ItemToAdd();
+        }
+
+        private class ItemToAdd : Target
+        {
+            public ItemToAdd()
+                : base(15, false, TargetFlags.None)
+            {
+            }
+
+            protected override void OnTarget(Mobile from, object targeted)
+            {
+                if (targeted is Item)
+                {
+                    Type t = targeted.GetType();
+                    foreach (Mobile m in World.Mobiles.Values)
+                    {
+                        m.AddToBackpack( (Item)Activator.CreateInstance(t) );
+                    }
+                }
+                else
+                {
+                    from.SendMessage("Ceci n'est pas un item.");
+                }
+            }
+        }
+    }
+}
