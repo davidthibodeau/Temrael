ac6f6ee347bddc2f5f0a931d4353ffa990c5f7a9
Trickster <trickster@uotemrael.com>
2015-01-14 11:19:31 -0500

    Modifications au basilic.

--- Scripts/Mobiles/Monsters/Basilic.cs
+++ Scripts/Mobiles/Monsters/Basilic.cs
@@ -8,7 +8,7 @@ namespace Server.Mobiles
     {
         [Constructable]
         public Basilic()
-            : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
+            : base(AIType.AI_Melee, FightMode.Closest, 7, 1, 0.2, 0.4)
         {
             Name = "Basilic";
             Body = 206;
@@ -28,16 +28,15 @@ namespace Server.Mobiles
             SetStam(250);
             SetArme(15, 23, 40);
 
-            SetResistance(ResistanceType.Physical, 20);
+            SetResistance(ResistanceType.Physical, 10);
             SetResistance(ResistanceType.Magical, 0);
 
-            SetSkill(SkillName.Infiltration, 90);
+            SetSkill(SkillName.Infiltration, 80);
             SetSkill(SkillName.ArmureNaturelle, 90);
             SetSkill(SkillName.Tactiques, 80);
             SetSkill(SkillName.Epee, 80);
             SetSkill(SkillName.Anatomie, 80);
             SetSkill(SkillName.Poursuite, 80);
-
         }
 
         public override int Hides { get { return 5; } }
@@ -47,8 +46,6 @@ namespace Server.Mobiles
             : base(serial)
         {
         }
-
-
 
         public override void Serialize(GenericWriter writer)
         {
