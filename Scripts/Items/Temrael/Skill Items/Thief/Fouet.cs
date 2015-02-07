1e847465bb299a89aea44baae5a4625ff9427bf0
frediiih <fred192837@hotmail.com>
2015-02-07 14:40:33 -0500

    Le fouet devrait être prêt.

--- Scripts/Items/Temrael/Skill Items/Thief/Fouet.cs
+++ Scripts/Items/Temrael/Skill Items/Thief/Fouet.cs
@@ -10,9 +10,9 @@ using Server.Engines.Combat;
 
 namespace Server.Items
 {
-    public abstract class BaseFouet : BaseWearable
+    public abstract class BaseFouet : BaseWearable, ICraftable, IScissorable
     {
-        protected const int Range = 7;
+        protected int m_Range;
         protected const double WaitTime = 10.0;                               //Wait time on individual target (seconds)
         protected const double CooldownTime = 5.0;                            //Cooldown time between each use (seconds)
         protected static HashSet<Mobile> MobilesList = new HashSet<Mobile>(); //Immuned mobiles
@@ -23,6 +23,63 @@ namespace Server.Items
         protected const double SnoopingScaling = 0.20;                        //Scaling off snooping
         protected const double SnoopingBonus = 20.0;                          //Bonus if snooping is at 100
 
+        private Mobile m_Crafter;
+        private string m_CrafterName;
+        private FouetQuality m_Quality;
+        private bool m_PlayerConstructed;
+        protected CraftResource m_Resource;
+
+        public enum FouetQuality
+        {
+            Low,
+            Regular,
+            Exceptional
+        }
+
+        #region PROPS
+        [CommandProperty(AccessLevel.Batisseur)]
+        public Mobile Crafter
+        {
+            get { return m_Crafter; }
+            set { m_Crafter = value; InvalidateProperties(); }
+        }
+
+        [CommandProperty(AccessLevel.Batisseur)]
+        public string CrafterName
+        {
+            get { return m_CrafterName; }
+            set { m_CrafterName = value; InvalidateProperties(); }
+        }
+
+        [CommandProperty(AccessLevel.Batisseur)]
+        public FouetQuality Quality
+        {
+            get { return m_Quality; }
+            set { m_Quality = value; InvalidateProperties(); }
+        }
+
+        [CommandProperty(AccessLevel.Batisseur)]
+        public bool PlayerConstructed
+        {
+            get { return m_PlayerConstructed; }
+            set { m_PlayerConstructed = value; }
+        }
+
+        [CommandProperty(AccessLevel.Batisseur)]
+        public CraftResource Resource
+        {
+            get { return m_Resource; }
+            set { m_Resource = value; Hue = CraftResources.GetHue(value); InvalidateProperties(); }
+        }
+
+        [CommandProperty(AccessLevel.Batisseur)]
+        public int Range
+        {
+            get { return m_Range; }
+            set { m_Range = value; InvalidateProperties(); }
+        }
+        #endregion
+
         public enum Sounds
         {
             Miss = 0x234 + 1,           //User misses the target
@@ -30,11 +87,16 @@ namespace Server.Items
             SuccessSteal = 0x534 + 3,   //User successfully steals the target's weapon
             Whip = 0x146 + 4            //User whips an unarmed target
         }
-
-        public BaseFouet(int id)
-            : base(id)
+        
+        public BaseFouet(int itemID)
+            : base(itemID)
         {
+            m_Quality = FouetQuality.Regular;
+            m_Crafter = null;
             NextUse = DateTime.MinValue;
+            Hue = CraftResources.GetHue(m_Resource);
+
+            this.Layer = Layer.OneHanded;
         }
 
         public BaseFouet(Serial serial) 
@@ -42,11 +104,66 @@ namespace Server.Items
         { 
         }
 
+        #region Serialization
+        private static void SetSaveFlag(ref SaveFlag flags, SaveFlag toSet, bool setIf)
+        {
+            if (setIf)
+                flags |= toSet;
+        }
+
+        private static bool GetSaveFlag(SaveFlag flags, SaveFlag toGet)
+        {
+            return ((flags & toGet) != 0);
+        }
+
+        [Flags]
+        private enum SaveFlag
+        {
+            None = 0x00000000,
+            Resource = 0x00000001,
+            Resistances = 0x00000002,
+            PlayerConstructed = 0x00000010,
+            Crafter = 0x00000020,
+            Quality = 0x00000040,
+            Range = 0x00000080,
+            CrafterName = 0x00000100
+
+        }
+
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
 
             writer.Write((int)0); // version
+
+            writer.Write((int)Layer);
+
+            SaveFlag flags = SaveFlag.None;
+
+            SetSaveFlag(ref flags, SaveFlag.Resource, true);
+            SetSaveFlag(ref flags, SaveFlag.PlayerConstructed, m_PlayerConstructed != false);
+            SetSaveFlag(ref flags, SaveFlag.Crafter, m_Crafter != null);
+            SetSaveFlag(ref flags, SaveFlag.CrafterName, m_CrafterName != null);
+            SetSaveFlag(ref flags, SaveFlag.Quality, m_Quality != FouetQuality.Regular);
+            SetSaveFlag(ref flags, SaveFlag.Range, Range != 7);
+
+
+            writer.WriteEncodedInt((int)flags);
+
+            if (GetSaveFlag(flags, SaveFlag.Resource))
+                writer.WriteEncodedInt((int)m_Resource);
+
+            if (GetSaveFlag(flags, SaveFlag.Crafter))
+                writer.Write((Mobile)m_Crafter);
+
+            if (GetSaveFlag(flags, SaveFlag.CrafterName))
+                writer.Write((string)m_CrafterName);
+
+            if (GetSaveFlag(flags, SaveFlag.Quality))
+                writer.WriteEncodedInt((int)m_Quality);
+
+            if (GetSaveFlag(flags, SaveFlag.Range))
+                writer.WriteEncodedInt((int)Range);
         }
 
         public override void Deserialize(GenericReader reader)
@@ -54,7 +171,41 @@ namespace Server.Items
             base.Deserialize(reader);
 
             int version = reader.ReadInt();
+
+            Layer = (Layer)reader.ReadInt();
+
+            SaveFlag flags = (SaveFlag)reader.ReadEncodedInt();
+
+            if (GetSaveFlag(flags, SaveFlag.Resource))
+                m_Resource = (CraftResource)reader.ReadEncodedInt();
+
+            if (GetSaveFlag(flags, SaveFlag.Crafter))
+                m_Crafter = reader.ReadMobile();
+
+            if (GetSaveFlag(flags, SaveFlag.CrafterName))
+                m_CrafterName = reader.ReadString();
+
+            if (GetSaveFlag(flags, SaveFlag.Quality))
+                m_Quality = (FouetQuality)reader.ReadEncodedInt();
+            else
+                m_Quality = FouetQuality.Regular;
+
+            if (GetSaveFlag(flags, SaveFlag.Range))
+                Range = reader.ReadEncodedInt();
+            else
+                Range = 7;
+
+            if (GetSaveFlag(flags, SaveFlag.PlayerConstructed))
+                m_PlayerConstructed = true;
+
+            Mobile parent = Parent as Mobile;
+
+            if (parent != null)
+            {
+                parent.CheckStatTimers();
+            }
         }
+        #endregion
         
         public override void OnDoubleClick(Mobile from)
         {
@@ -62,6 +213,8 @@ namespace Server.Items
                 from.SendMessage("Vous ne pouvez pas utiliser le fouet maintenant.");
             else if (from.Dex < 65)
                 from.SendMessage("Vous n'êtes pas assez à l'aise pour utiliser le fouet.");
+            else if (from.FindItemOnLayer(Layer.OneHanded) != this)
+                from.SendMessage("Vous devez avoir le fouet en main pour l'utiliser.");
             else
                 from.Target = new InternalTarget(this, Range);
         }
@@ -85,6 +238,14 @@ namespace Server.Items
             double chances = GetChances(atk, def);
             NextUse = DateTime.Now.AddSeconds(CooldownTime);
             Item weapon = Weapon(def);
+
+            if (def.Spell != null)
+            {
+                if (def.Spell.IsCasting && (chances - def.Skills[SkillName.Concentration].Value / 1000) >= Utility.RandomDouble())
+                {
+                    def.DisruptiveAction();
+                }
+            }
 
             if (weapon.Layer == Layer.TwoHanded) //Target has a two-handed weapon
             {
@@ -141,7 +302,6 @@ namespace Server.Items
             atk.RevealingAction();
             def.RevealingAction();
             atk.DisruptiveAction();
-            def.DisruptiveAction();
             CombatStrategy.GetStrategy(atk).ResetAttackAfterCast(atk);
             CombatStrategy.GetStrategy(def).ResetAttackAfterCast(def);
         }
@@ -253,6 +413,79 @@ namespace Server.Items
                 }
             }
         }
+
+        #region IScissorable
+        public virtual bool Scissor(Mobile from, Scissors scissors)
+        {
+            if (!IsChildOf(from.Backpack))
+            {
+                from.SendLocalizedMessage(502437); // Items you wish to cut must be in your backpack.
+                return false;
+            }
+
+            CraftSystem system = DefTailoring.CraftSystem;
+
+            CraftItem item = system.CraftItems.SearchFor(GetType());
+
+            if (item != null && item.Resources.Count == 1 && item.Resources.GetAt(0).Amount >= 2)
+            {
+                try
+                {
+                    Type resourceType = null;
+
+                    CraftResourceInfo info = CraftResources.GetInfo(m_Resource);
+
+                    if (info != null && info.ResourceTypes.Length > 0)
+                        resourceType = info.ResourceTypes[0];
+
+                    if (resourceType == null)
+                        resourceType = item.Resources.GetAt(0).ItemType;
+
+                    Item res = (Item)Activator.CreateInstance(resourceType);
+
+                    ScissorHelper(from, res, m_PlayerConstructed ? (item.Resources.GetAt(0).Amount / 2) : 1);
+
+                    res.LootType = LootType.Regular;
+
+                    return true;
+                }
+                catch
+                {
+
+                }
+            }
+
+            from.SendLocalizedMessage(502440); // Scissors can not be used on that to produce anything.
+            return false;
+        }
+        #endregion
+
+        #region ICraftable
+        public virtual int OnCraft(int quality, bool makersMark, Mobile from, CraftSystem craftSystem, Type typeRes, BaseTool tool, CraftItem craftItem, int resHue)
+        {
+            Quality = (FouetQuality)quality;
+
+            Crafter = from;
+
+            Type resourceType = typeRes;
+
+            if (resourceType == null)
+                resourceType = craftItem.Resources.GetAt(0).ItemType;
+
+            Resource = CraftResources.GetFromType(resourceType);
+            PlayerConstructed = true;
+
+            CraftContext context = craftSystem.GetContext(from);
+
+            RareteInit.InitItem(this, quality, Crafter);
+
+            if (context != null && context.DoNotColor)
+                Hue = 0;
+
+            return quality;
+        }
+
+        #endregion
     }
 
     public class FouetCuir : BaseFouet
@@ -262,8 +495,8 @@ namespace Server.Items
             : base(0x166E)
         {
             Weight = 5.0;
-            Layer = Layer.OneHanded;
             Name = "Fouet en cuir";
+            Range = 7;
         }
 
         public FouetCuir(Serial serial)
