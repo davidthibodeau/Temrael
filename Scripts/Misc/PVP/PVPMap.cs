6baab466ca681ddb3434d6f027ad1b15132f1de8
Zilmerx <hex404@gmail.com>
2015-06-04 18:02:42 -0400

    Mise en place de 3 autres maps.

--- Scripts/Misc/PVP/PVPMap.cs
+++ Scripts/Misc/PVP/PVPMap.cs
@@ -11,6 +11,7 @@ namespace Server.Misc.PVP
         // Ne pas changer l'ordre des maps pour aucune raison : Pourrait causer des bugs de serialize. Il faut ajouter à la fin.
         public static List<PVPMap> MapList = new List<PVPMap>
         {
+            #region Map defs
             /* 0 */ new PVPMap("Ville de Vénore", Map.Ilshenar, 4, new List<Point3D>()
             {
                 // Spawnpoints.
@@ -26,8 +27,58 @@ namespace Server.Misc.PVP
                 new Point3D(184, 148, -54),
                 new Point3D(228, 150, -59),
                 new Point3D(218, 110, -59)
+            }),
+
+            /* 1 */ new PVPMap("Fort de Kurok", Map.Ilshenar, 2, new List<Point3D>()
+            {
+                // Spawnpoints.
+                new Point3D(487, 175, -50),
+                new Point3D(508, 116, -80),
+
+                new Point3D(409, 191, -81),
+                new Point3D(426, 208, -78),
+                new Point3D(434, 148, -10),
+                new Point3D(445, 123, -50),
+                new Point3D(456, 138, -50),
+                new Point3D(476, 152, -30),
+                new Point3D(500, 149, -82)
+            }),
+
+            /* 2 */ new PVPMap("Cîme aux faucons", Map.Ilshenar, 3, new List<Point3D>()
+            {
+                // Spawnpoints.
+                new Point3D(838, 147, -80),
+                new Point3D(749, 109, -21),
+                new Point3D(706, 199, -80),
+
+                new Point3D(749, 163, -80),
+                new Point3D(740, 137, -58),
+                new Point3D(798, 149, -38),
+                new Point3D(795, 127, 4),
+                new Point3D(780, 193, -81),
+                new Point3D(803, 164, -58),
+                new Point3D(706, 174, -80)
+            }),
+
+            /* 3 */ new PVPMap("Le bazar de Mirage", Map.Ilshenar, 2, new List<Point3D>()
+            {
+                // Spawnpoints.
+                new Point3D(1101, 196, -30),
+                new Point3D(1103, 115, -80),
+
+                new Point3D(1098, 152, -31),
+                new Point3D(1067, 155, -25),
+                new Point3D(1066, 165, -30),
+                new Point3D(1076, 164, -30),
+                new Point3D(1097, 163, -10),
+                new Point3D(1097, 180, -5),
+                new Point3D(1069, 190, -30),
+                new Point3D(1069, 184, -30),
+                new Point3D(1105, 150, -30)
             })
+            #endregion
         };
+
 
         #region Membres
         private String m_Name;                  // Nom du terrain de bataille.
@@ -58,7 +109,7 @@ namespace Server.Misc.PVP
         #endregion
         #endregion
 
-        PVPMap(String name, Map map, int nbTeamSpawnPoints, List<Point3D> spawnpoints)
+        private PVPMap(String name, Map map, int nbTeamSpawnPoints, List<Point3D> spawnpoints)
         {
             m_Name = name;
             m_Map = map;
